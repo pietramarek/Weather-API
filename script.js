2fafdd44a23695dadf9e40107a0e917a2b1e9774
pietramarek <pietramarek@gmail.com>
2023-05-12 09:34:59 +0200

    API different file

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-const API_KEY="ffdee663130bb47b6b8b4796e39ea0a2";
+import { API_KEY } from "./API_Key.js";
 
 const cityName = document.getElementById("city");
 const result = document.getElementById("result");
 
 document.getElementById("btn").addEventListener("click", async () => {
-    try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=${API_KEY}`);
-        const data = await response.json();
-        console.log(data);
-        /* const { name } = data;
-        const { feels_like } = data.main;
-        const { main } = data.weather[0]; */
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=${API_KEY}&lang=pl&units=metric`
+    );
+    const data = await response.json();
+    console.log(data);
 
-        document.getElementById("city").innerHTML = data;  
-        document.getElementById("weather").innerHTML = data.weather[0].description;  
-        document.getElementById("temp").innerHTML = data.main.temp;  
-        document.getElementById("cloud").innerHTML = data.weather[0].main;  
-        document.getElementById("wind").innerHTML = data.wind.speed;  
-
-       /*  result.innerHTML = `
-        <div class="city">${name}</div>
-        <div class="temp">${Math.round(feels_like - 273.15)}°C</div>
-        <div class="weather">${main}</div>
-        `; */
-    } catch (error) {
-        console.error(error);
-    }
+    document.getElementById("city").innerHTML = data;
+    document.getElementById("weather").innerHTML = data.weather[0].description;
+    document.getElementById("temp").innerHTML = data.main.temp;
+    document.getElementById("cloud").innerHTML = data.weather[0].main;
+    document.getElementById("wind").innerHTML = data.wind.speed;
+  } catch (error) {
+    console.error(error);
+  }
 });
-
