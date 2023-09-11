3dc5a689b572474d5b2f4654723ee3cdf9acc66c
pietramarek <pietramarek@gmail.com>
2023-09-11 22:01:16 +0200

    Adding API icons, 3 hr API call testing

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const pressure = document.getElementById("preasure");
 const bialystok = document.getElementById("bialystok");
 const warszawa = document.getElementById("warszawa");
 const londyn = document.getElementById("londyn");
+const forecast = document.getElementById("forecast");
 
 // domyślne przyciski miast
 bialystok.addEventListener("click", () => {
@@ -25,40 +26,38 @@ londyn.addEventListener("click", () => {
 
 document.getElementById("btn").addEventListener("click", async () => {
   container.style.display = "block";
+
   try {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&lang=pl&units=metric&appid=${API_KEY}`
     );
+    const response2 = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${cityName.value}&cnt=3&appid=${API_KEY}`
+    );
+
+    const data2 = await response2.json();
+    console.log(data2.list[0].weather[0].description);
+
+    console.log(data2.list[0].dt_txt);
+    console.log(data2.list[0].main.temp);
+    console.log(data2.list[0].weather[0].description);
+    console.log(data2.list[0].weather[0].icon);
+
+    console.log(data2.list[1].dt_txt);
+    console.log(data2.list[1].main.temp);
+    console.log(data2.list[1].weather[0].description);
+
+    console.log(data2.list[2].dt_txt);
+    console.log(data2.list[2].main.temp);
+    console.log(data2.list[2].weather[0].description);
 
     const data = await response.json();
-    //console.log(data);
-
     weather.innerHTML = data.weather[0].description;
+    image.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
     temp.innerHTML = "Temperatura: " + Math.round(data.main.temp) + "°C";
     wind.innerHTML = "Prędkość wiatru: " + data.wind.speed + "m/s";
     pressure.innerHTML = "Ciśnienie: " + data.main.pressure + " hPa";
     image.style.display = "block";
-
-    if (data.weather[0].description === "bezchmurnie") {
-      image.src = "images/sunny.png";
-    } else if (
-      data.weather[0].description === "zachmurzenie duże" ||
-      data.weather[0].description == "pochmurnie"
-    ) {
-      image.src = "images/cloudy.png";
-    } else if (
-      data.weather[0].description == "zachmurzenie umiarkowane" ||
-      data.weather[0].description == "zachmurzenie małe" ||
-      data.weather[0].description == "zachmurzenie duże"
-    ) {
-      image.src = "images/partlycloudy.png";
-    } else if (
-      data.weather[0].description == "słabe opady deszczu" ||
-      data.weather[0].description == "umiarkowane opady deszczu" ||
-      data.weather[0].description == "mocne opady deszczu"
-    ) {
-      image.src = "images/rainy.png";
-    } else image.style.display = "none";
   } catch (error) {
     container.style.display = "none";
     image.style.display = "none";
